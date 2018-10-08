96d1127ebbc3579ec60c61249e09cc8c498ae821
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2018-10-08 09:08:33 +0530

    add hello worl text

--- basic-hello-world/App.js
+++ basic-hello-world/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Text>Hello World</Text>
+        <Text>Hello Manjula</Text>
+        <TextInput
+            style={{height: 40}}
+            placeholder="Type something!"
+          />
       </View>
     );
   }

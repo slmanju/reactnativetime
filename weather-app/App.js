c9395641845a6c9fd2f49dea2932ebe571170add
Manjula Jayawardana <manjulajayawardana@gmail.com>
2018-10-18 09:17:05 +0530

    weather-app textinput with keyboard avoiding view

--- weather-app/App.js
+++ weather-app/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View, Platform } from 'react-native';
+import { StyleSheet, Text, View, Platform, TextInput, KeyboardAvoidingView } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <KeyboardAvoidingView style={ styles.container } behavior="padding">
         <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
         <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
         <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
-      </View>
+        <TextInput
+                  autoCorrect={ false }
+                  placeholder="Search any city"
+                  placeholderTextColor="white"
+                  style={ styles.textInput }
+                  clearButtonMode="always"
+                  />
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -30,4 +37,14 @@ const styles = StyleSheet.create({
   smallText: {
     fontSize: 18,
   },
+  textInput: {
+    backgroundColor: '#666',
+    color: 'white',
+    height: 40,
+    width: 300,
+    marginTop: 20,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    alignSelf: 'center',
+  }
 });
